4a8683e1dac0e7147b2753b142a73aee8a515339
Md. Ishtiaq Ahamed Fahim <ishtiaq.fahim@northsouth.edu>
2022-09-23 16:01:40 +0600

    Just checking to read file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
+use std::io::prelude::*;
+use std::io::BufReader;
+use std::fs::File;
 
 fn main() {
-
+    let file = File::open("img/text.txt").expect("Unable to open file");
+    let mut reader = BufReader::new(file);
+    let mut str=String::new();
+    reader.read_line(&mut str).expect("Unable to read file");
+    println!("{}",str);
 }
