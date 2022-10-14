9ea47ad12a67a43f487bbffd3cda60fe9fdfeebe
Md. Ishtiaq Ahamed Fahim <ishtiaq.fahim@northsouth.edu>
2022-10-14 16:19:11 +0600

    DFS next

--- src/floor_plan.rs
+++ src/floor_plan.rs
@@ -1,3 +1,4 @@
+use std::iter::Map;
 use std::mem::transmute;
 use std::thread::current;
 use image::{DynamicImage, GenericImage, GenericImageView, Rgba};
@@ -7,16 +8,20 @@ pub struct Area {
     img: DynamicImage,
     width: usize,
     height: usize,
-    tolerance: i32,
+    tolerance: u8,
     maybe_font_height: u16,
     maybe_font_width: u16,
+    dark_color_sum: u16,
     color_to_ignore: [u8; 4],
     pub y_pre_sum_matrix: Vec<Vec<u16>>,
+    pub x_pre_sum_matrix: Vec<Vec<u16>>,
+    pub wall_matrix: Vec<Vec<bool>>,
     pub dir: [(i32, i32); 8],
+    pub prominent_dark_color: Vec<[u8; 4]>,
 }
 
 impl Area {
-    pub fn new(path: &str, tolerance: i32) -> Area {
+    pub fn new(path: &str, tolerance: u8) -> Area {
         let img = image::open(path).expect("Cant open the image");
         let width = img.width() as usize;
         let height = img.height() as usize;
@@ -29,26 +34,36 @@ impl Area {
             maybe_font_height: 12,
             maybe_font_width: 3,
             color_to_ignore: [250, 250, 250, 250],
+            dark_color_sum: 300,
             y_pre_sum_matrix: vec![vec![0; height as usize]; width as usize],
+            x_pre_sum_matrix: vec![vec![0; height as usize]; width as usize],
+            wall_matrix: vec![vec![false; height as usize]; width as usize],
             dir: [(1, 0), (1, -1), (0, 1), (-1, 1), (-1, 0), (0, -1), (-1, -1), (1, 1)],
+            prominent_dark_color: Vec::new(),
         }
     }
 
     pub fn pixel_is_similar_with_tolerance(&self, current_pixel: [u8; 4], pixel_to_compare: [u8; 4]) -> bool {
         let mut changed_found_on_colors: i8 = 0;
         for i in 0..3 {
-            if (current_pixel[i] as i32 - pixel_to_compare[i] as i32).abs() > self.tolerance {
+            let mut dif: u8 = 0;
+            if current_pixel[i] > pixel_to_compare[i] {
+                dif = current_pixel[i] - pixel_to_compare[i];
+            } else {
+                dif = pixel_to_compare[i] - current_pixel[i];
+            }
+            if dif > self.tolerance {
                 changed_found_on_colors += 1;
             }
         }
         changed_found_on_colors == 0
     }
 
-    pub fn y_matrix_pre_sum_from_bottom_up(&mut self) {
+    pub fn create_y_matrix_pre_sum_from_bottom_up(&mut self) {
         for x in 0..self.width {
             let mut pre = self.img.get_pixel(x as u32, self.height as u32 - 1).0;
             let mut y = self.height - 2;
-            while y > 0 {
+            while y >= 0 {
                 let current_pixel = self.img.get_pixel(x as u32, y as u32).0;
                 if self.pixel_is_similar_with_tolerance(current_pixel, pre) {
                     self.y_pre_sum_matrix[x][y] = self.y_pre_sum_matrix[x][y + 1] + 1;
@@ -59,11 +74,68 @@ impl Area {
         }
     }
 
+    pub fn create_x_matrix_pre_sum_from_bottom_up(&mut self) {
+        for y in 0..self.height {
+            let mut pre = self.img.get_pixel(0, y as u32).0;
+            let mut x = self.width - 2;
+            while x >= 0 {
+                let current_pixel = self.img.get_pixel(x as u32, y as u32).0;
+                if self.pixel_is_similar_with_tolerance(current_pixel, pre) {
+                    self.x_pre_sum_matrix[x][y] = self.x_pre_sum_matrix[x + 1][y] + 1;
+                }
+                pre = current_pixel;
+                x -= 1;
+            }
+        }
+    }
+
+
     pub fn create_color_sets(&mut self) {
         //TODO: create color sets
     }
 
+    pub fn gather_prominent_colors(&mut self) {
+        let rather_black = self.tolerance as u8;
+        self.prominent_dark_color.push([rather_black, rather_black, rather_black, 255]);
+    }
+
+    pub fn create_wall_matrix(&mut self) {
+        for y in 0..self.height {
+            for x in 0..self.width {
+                let current_pixel = self.img.get_pixel(x as u32, y as u32).0;
+                if self.pixel_is_similar_with_tolerance(current_pixel, self.prominent_dark_color[0]) {
+                    self.wall_matrix[x][y] = true;
+                }
+            }
+        }
+    }
+
+    pub fn get_rid_of_all_light_color(&mut self) {
+        if self.path.contains(".jpg") {
+            self.path = self.path.replace(".jpg", ".png");
+        }
+        let path = self.path.clone().replace(".png", "_without_light_color.png");
+        let mut img = DynamicImage::new_rgb8(self.width as u32, self.height as u32);
+        for y in 0..self.height {
+            for x in 0..self.width {
+                let current_pixel_Rgba = self.img.get_pixel(x as u32, y as u32);
+                let current_pixel = current_pixel_Rgba.0;
+                let sum = current_pixel[0] as u16 + current_pixel[1] as u16 + current_pixel[2] as u16;
+                if sum < self.dark_color_sum {
+                    img.put_pixel(x as u32, y as u32, current_pixel_Rgba);
+                } else {
+                    img.put_pixel(x as u32, y as u32, Rgba([255, 255, 255, self.tolerance]));
+                }
+            }
+        }
+        img.save(path).expect("Cant save the image");
+    }
+
+
     pub fn separate_by_color(&mut self, color: [u8; 4]) {
+        if self.path.contains(".jpg") {
+            self.path = self.path.replace(".jpg", ".png");
+        }
         let path = self.path.clone().replace(".png", "_separated_by_color.png");
         let mut img: DynamicImage = DynamicImage::new_rgb8(self.width as u32, self.height as u32);
         for x in 0..self.width {
@@ -89,16 +161,6 @@ impl Area {
             }
         }
         img.save(path).expect("Cant save the image");
-    }
-
-    pub fn mean_pixel_is_smaller_than_tolerance_if_last_was_mismatch(&self, first: [u8; 4], mid: [u8; 4], last: [u8; 4]) -> bool {
-        let mut changed_found_on_colors: i8 = 0;
-        for i in 0..3 {
-            if (((first[i] as i32 + last[i] as i32) / 2) - (mid[i] as i32)).abs() > self.tolerance {
-                changed_found_on_colors += 1;
-            }
-        }
-        changed_found_on_colors == 0
     }
 
     pub fn dominant_outer_color(&self) -> [u8; 4] {
--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,8 @@ use std::io::{stdin, Write};
 mod floor_plan;
 
 fn main() {
-    let  pic_path="img/simple_floor_plan_1.png";
+    let  pic_path="img/simple_floor_plan_2.jpg";
     let mut floor_plan = floor_plan::Area::new(pic_path, 20);
-    floor_plan.y_matrix_pre_sum_from_bottom_up();
-    floor_plan.y_heat_map();
-    floor_plan.separate_by_color([86, 93, 96, 255]);
-
-
+    floor_plan.get_rid_of_all_light_color();
 }
 
